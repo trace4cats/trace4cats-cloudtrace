1dfb21deb24007c06fe90be4b2d9163e85580bf4
Scala Steward <me@scala-steward.org>
2021-07-07 16:54:34 +0200

    Update google-cloud-trace to 1.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val circe = "0.14.1"
     val collectionCompat = "2.4.4"
     val googleCredentials = "0.26.0"
-    val googleCloudTrace = "1.4.1"
+    val googleCloudTrace = "1.4.2"
     val http4s = "0.23.0-RC1"
     val jwt = "3.18.1"
     val log4cats = "2.1.1"
