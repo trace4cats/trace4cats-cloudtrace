bf208ae9280785f94cd5c82b1517a86b42e75b54
Scala Steward <me@scala-steward.org>
2021-08-19 06:27:17 +0200

    Reformat with scalafmt 3.0.0

--- modules/stackdriver-http-exporter/src/main/scala/io/janstenpickle/trace4cats/stackdriver/oauth/OAuth.scala
+++ modules/stackdriver-http-exporter/src/main/scala/io/janstenpickle/trace4cats/stackdriver/oauth/OAuth.scala
@@ -9,10 +9,14 @@ import scala.concurrent.duration.FiniteDuration
 trait OAuth[F[_]] {
 
   /** Based on https://developers.google.com/identity/protocols/OAuth2ServiceAccount
-    * @param iss The email address of the service account.
-    * @param scope A space-delimited list of the permissions that the application requests.
-    * @param exp The expiration time of the assertion, specified as milliseconds since 00:00:00 UTC, January 1, 1970.
-    * @param iat The time the assertion was issued, specified as milliseconds since 00:00:00 UTC, January 1, 1970.
+    * @param iss
+    *   The email address of the service account.
+    * @param scope
+    *   A space-delimited list of the permissions that the application requests.
+    * @param exp
+    *   The expiration time of the assertion, specified as milliseconds since 00:00:00 UTC, January 1, 1970.
+    * @param iat
+    *   The time the assertion was issued, specified as milliseconds since 00:00:00 UTC, January 1, 1970.
     */
   def authenticate(iss: String, scope: String, exp: Instant, iat: Instant): F[Option[AccessToken]]
 
