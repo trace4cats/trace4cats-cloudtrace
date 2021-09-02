130df5b28119e488ac42f256312a7c440d910770
Scala Steward <me@scala-steward.org>
2021-09-02 02:02:34 +0200

    Update http4s-blaze-client, http4s-circe to 0.23.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val collectionCompat = "2.5.0"
     val googleCredentials = "1.1.0"
     val googleCloudTrace = "2.0.3"
-    val http4s = "0.23.1"
+    val http4s = "0.23.2"
     val jwt = "3.18.1"
     val log4cats = "2.1.1"
 
