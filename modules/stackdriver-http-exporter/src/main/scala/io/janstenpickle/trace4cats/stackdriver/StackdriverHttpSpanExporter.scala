7ebfa43677712cfe16839477661feaebc47c75a0
Henry <henry.dev.acc@gmail.com>
2021-11-28 21:40:57 +0100

    Remove Blaze client dependency, take a typed Uri in constructors

--- modules/stackdriver-http-exporter/src/main/scala/io/janstenpickle/trace4cats/stackdriver/StackdriverHttpSpanExporter.scala
+++ modules/stackdriver-http-exporter/src/main/scala/io/janstenpickle/trace4cats/stackdriver/StackdriverHttpSpanExporter.scala
@@ -1,11 +1,11 @@
 package io.janstenpickle.trace4cats.stackdriver
 
 import cats.Foldable
-import cats.effect.kernel.{Async, Resource, Temporal}
+import cats.effect.kernel.{Async, Temporal}
+import cats.syntax.either._
 import cats.syntax.flatMap._
-import cats.syntax.functor._
 import cats.syntax.foldable._
-import org.typelevel.log4cats.Logger
+import cats.syntax.functor._
 import io.janstenpickle.trace4cats.`export`.HttpSpanExporter
 import io.janstenpickle.trace4cats.kernel.SpanExporter
 import io.janstenpickle.trace4cats.model.Batch
@@ -23,30 +23,12 @@ import io.janstenpickle.trace4cats.stackdriver.project.{
 import org.http4s.Uri
 import org.http4s.circe.CirceEntityCodec._
 import org.http4s.client.Client
-import org.http4s.blaze.client.BlazeClientBuilder
+import org.typelevel.log4cats.Logger
 
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.ExecutionContext
 
 object StackdriverHttpSpanExporter {
   private final val base = "https://cloudtrace.googleapis.com/v2/projects"
-
-  def serviceAccountBlazeClient[F[_]: Async: Logger, G[_]: Foldable](
-    projectId: String,
-    serviceAccountPath: String,
-    ec: Option[ExecutionContext] = None
-  ): Resource[F, SpanExporter[F, G]] = for {
-    client <- ec.fold(BlazeClientBuilder[F])(BlazeClientBuilder[F].withExecutionContext).resource
-    exporter <- Resource.eval(apply[F, G](projectId, serviceAccountPath, client))
-  } yield exporter
-
-  def blazeClient[F[_]: Async: Logger, G[_]: Foldable](
-    serviceAccountName: String = "default",
-    ec: Option[ExecutionContext] = None
-  ): Resource[F, SpanExporter[F, G]] = for {
-    client <- ec.fold(BlazeClientBuilder[F])(BlazeClientBuilder[F].withExecutionContext).resource
-    exporter <- Resource.eval(apply[F, G](client, serviceAccountName))
-  } yield exporter
 
   def apply[F[_]: Async: Logger, G[_]: Foldable](
     projectId: String,
@@ -75,21 +57,21 @@ object StackdriverHttpSpanExporter {
     for {
       cachedTokenProvider <- CachedTokenProvider(tokenProvider)
       projectId <- projectIdProvider.projectId
-      exporter <- HttpSpanExporter[F, G, model.Batch](
-        client,
-        s"$base/$projectId/traces:batchWrite",
-        (batch: Batch[G]) =>
-          model.Batch(
-            batch.spans
-              .foldLeft(ListBuffer.empty[model.Span]) { (buf, span) =>
-                buf += model.Span.fromCompleted(projectId, span)
-              }
-              .toList
-          ),
-        (uri: Uri) =>
-          cachedTokenProvider.accessToken.map { token =>
-            uri.withQueryParam("access_token", token.access_token)
-          }
-      )
-    } yield exporter
+      uri <- Uri.fromString(s"$base/$projectId/traces:batchWrite").liftTo[F]
+    } yield HttpSpanExporter[F, G, model.Batch](
+      client,
+      uri,
+      (batch: Batch[G]) =>
+        model.Batch(
+          batch.spans
+            .foldLeft(ListBuffer.empty[model.Span]) { (buf, span) =>
+              buf += model.Span.fromCompleted(projectId, span)
+            }
+            .toList
+        ),
+      (uri: Uri) =>
+        cachedTokenProvider.accessToken.map { token =>
+          uri.withQueryParam("access_token", token.access_token)
+        }
+    )
 }
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val scala3 = "3.1.0"
 
     val trace4cats = "0.12.0"
-    val trace4catsExporterHttp = "0.12.0"
+    val trace4catsExporterHttp = "0.12.0+41-8ce63144"
 
     val circe = "0.14.1"
     val collectionCompat = "2.6.0"
