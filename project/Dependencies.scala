9e48538ed0ad34c6404c41a0af62a366af9c348b
Scala Steward <scala_steward@virtuslab.com>
2022-07-19 18:54:24 +0000

    Update log4cats-slf4j to 2.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val googleCloudTrace = "2.2.0"
     val http4s = "0.23.12"
     val jwt = "3.19.2"
-    val log4cats = "2.3.2"
+    val log4cats = "2.4.0"
 
     val kindProjector = "0.13.2"
     val betterMonadicFor = "0.3.1"
