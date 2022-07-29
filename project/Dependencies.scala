9a97240578fc36a388ca273fbc082ad8bef0a761
Scala Steward <scala_steward@virtuslab.com>
2022-07-29 21:07:17 +0000

    Update trace4cats-exporter-http to 0.13.1+26-3c5516ed

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val scala3 = "3.1.3"
 
     val trace4cats = "0.13.1+67-a7d9b849"
-    val trace4catsExporterHttp = "0.13.1+14-639b5d65"
+    val trace4catsExporterHttp = "0.13.1+26-3c5516ed"
 
     val circe = "0.14.2"
     val collectionCompat = "2.8.0"
