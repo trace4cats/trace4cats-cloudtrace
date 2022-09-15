3d920db514ff79facab5e7e5c013d944dd70a614
Scala Steward <scala_steward@virtuslab.com>
2022-09-15 21:02:16 +0000

    Update log4cats-slf4j to 2.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val http4sBlaze = "0.23.12"
 
     val jwt = "4.0.0"
-    val log4cats = "2.4.0"
+    val log4cats = "2.5.0"
 
     val kindProjector = "0.13.2"
     val betterMonadicFor = "0.3.1"
